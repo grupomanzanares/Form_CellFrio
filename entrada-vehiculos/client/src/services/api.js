345f581ad0872791f98d7caa4bba1c85b784c3e5
Cristian Paez <cpaezariza@gmail.com>
2026-04-08 15:21:29 -0500

    pasos finales

--- entrada-vehiculos/client/src/services/api.js
+++ entrada-vehiculos/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api'
+  baseURL: 'https://gmanzanares.com.co/cellfrio'
 })
 
 api.interceptors.request.use((config) => {
